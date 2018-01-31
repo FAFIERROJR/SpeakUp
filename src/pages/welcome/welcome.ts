fe9fdff18721046efd8d2ff8f37ca44034c5b62f
Francisco Fierro <f.a.fierrojr@gmail.com>
2018-01-30 23:46:44 -0800

    Added newPage

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -7,6 +7,7 @@ import{TestingPage} from '../testing/testing'
 import { HelloIonicPage } from '../hello-ionic/hello-ionic';
 import { ChatroomPage } from '../chatroom/chatroom';
 import { PARAMETERS } from '@angular/core/src/util/decorators';
+import { NewPage } from '../new/new';
 
 @Component({
     selector: 'page-Welcome',
@@ -34,5 +35,9 @@ export class WelcomePage{
 
     goChatroomPage(){
         this.navCtrl.push(ChatroomPage, {'chatroomID': 0});
+    }
+
+    goNewPage(){
+        this.navCtrl.push(NewPage);
     }
 }
